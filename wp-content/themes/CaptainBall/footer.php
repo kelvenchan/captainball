03e18bb5a3ea2dc7e52b4670ac05d3bbb1ee735a
MSI\User <pheiyuen.chia@thetechyhub.com>
2017-05-25 15:05:45 +0800

    updated plugin

---
+++ wp-content/themes/CaptainBall/footer.php
@@ -0,0 +1,21 @@
+<!-- Footer -->
+	<footer class="container-fuild">
+		<div class="container small-contain">
+			<div class="footer-links">
+				<a href="https://www.facebook.com/copaibasports" class=""><i class="fa fa-facebook-square fa-2x" aria-hidden="true"></i></a>
+				<a href="" class=""><i class="fa fa-twitter-square fa-2x" aria-hidden="true"></i></a>
+				<a href="" class=""><i class="fa fa-instagram fa-2x" aria-hidden="true"></i></a>
+			</div>
+			<h6>&copy; 2017 by CopaIba All Rights Reserved</h6>
+		</div>
+	</footer>
+
+  <script src="https://ajax.googleapis.com/ajax/libs/jquery/3.1.1/jquery.min.js"></script>
+  <script src="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/js/bootstrap.min.js"></script>
+  <script src="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/js/materialize.min.js"></script>
+  <script src="js/jquery.scrollTo.min.js" type="text/javascript"></script>
+  <script src="js/modernizr.js"></script>
+  <script src="js/script.js" type="text/javascript"></script>
+
+</body>
+</html>
---
+++ wp-content/themes/CaptainBall/header.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>CaptainBall</title>
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no" />
+  <link href="images/favicon.ico" rel="shortcut icon" type="image/x-icon">
+
+  <link href="https://fonts.googleapis.com/css?family=Josefin+Sans|Montserrat:200,300,400" rel="stylesheet">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
+  <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/0.98.2/css/materialize.min.css">
+  <link href="style.css" rel="stylesheet" type="text/css" />
+</head>
+
+<body>
+	
+	<div class="main">
+
+<!-- Navigation -->
+    <nav class="cd-stretchy-nav">
+      <a class="cd-nav-trigger" href="#0">
+        <span aria-hidden="true"></span>
+      </a>
+
+      <ul>
+        <li><a href="#def"><span>Definition</span><i class="fa fa-search" aria-hidden="true"></i></a></li>
+        <li><a href="#equipments"><span>Equipments</span><i class="fa fa-sign-language" aria-hidden="true"></i></a></li>
+        <li><a href="#play"><span>How to Play</span><i class="fa fa-futbol-o" aria-hidden="true"></i></a></li>
+        <li><a href="#rules"><span>Game Rules</span><i class="fa fa-file-text" aria-hidden="true"></i></a></li>
+        <li><a href="#join-us"><span>Participate</span><i class="fa fa-phone" aria-hidden="true"></i></a></li>
+        <li><a href="#topbar"><span>Top</span><i class="fa fa-arrow-up" aria-hidden="true"></i></a></li>
+      </ul>
+
+      <span aria-hidden="true" class="stretchy-nav-bg"></span>
+    </nav>
